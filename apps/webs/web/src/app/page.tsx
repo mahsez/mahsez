c28ecf9b43e392ab3c4281243a8cc8567d36550b
Abdullah Al Masud <abdullahalmasud772@gmail.com>
2026-02-28 01:38:19 +0600

    change package manager npm to pnpm

--- apps/webs/web/src/app/page.tsx
+++ apps/webs/web/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome @mahsez/web 👋 With test new
+              Welcome @mahsez/web 👋 With test new2
             </h1>
           </div>
 
